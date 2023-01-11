3932db2ae7bfbcedbd3a4db1f4b1da3432bc5e83
AndresFelipeCamachoCuellar <71158195+AndresFelipeCamachoCuellar@users.noreply.github.com>
2023-01-11 11:45:09 -0500

    BackEnd Terminado

---
+++ AirLine/AirLineApi/Controllers/AirplaneController.cs
@@ -0,0 +1,208 @@
+﻿using AirLineApi.Models;
+using AirLineApi.Util;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace AirLineApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AirplaneController : ControllerBase
+    {
+        [HttpGet("/[controller]/GetAirplanes")]
+        public Response GetAirplanes()
+        {
+            Response objResponse = new Response()
+            {
+                Resp = true
+            };
+            try
+            {
+                Connection objConnection = new Connection(Config.DBPass, Config.DBServer, Config.DBName, Config.DBUser);
+
+
+                string[] arrParams = new string[] { };
+                string[] arrValues = new string[] { };
+
+                string strQuery = @"SELECT AIP.AirPlaneID, AIP.CompanyID, COM.Name AS NAME_COMPANY, AIP.Name AS NAME_AIRPLANE, AIP.Status  
+                                    FROM AirPlane AIP 
+                                    INNER JOIN Company COM ON AIP.CompanyID = COM.CompanyID";
+
+                ResponseDB dbResponse = objConnection.getRespFromQuery(0, 500, "", strQuery, arrParams, arrValues, "COM.Name", "DataTable");
+
+                if (dbResponse.Resp)
+                {
+                    if (dbResponse.Count > 0)
+                    {
+                        objResponse.Count = dbResponse.Count;
+                        objResponse.Data = JsonConvert.SerializeObject(dbResponse.dtResult, Formatting.None);
+                    }
+                    else
+                    {
+                        objResponse.Resp = false;
+                        objResponse.Type = "Error";
+                        objResponse.Msg = "No se encontro ningun usuario asociado a tu empresa";
+                    }
+                }
+                else
+                {
+                    throw new Exception(dbResponse.Msg);
+                }
+
+            }
+            catch (Exception exc)
+            {
+                objResponse.Resp = false;
+                objResponse.Type = "Error";
+                objResponse.Msg = "Ocurrió un error inesperado, por favor consulte al administrador";
+                objResponse.Detail = exc.Message;
+            }
+
+            return objResponse;
+        }
+
+        [HttpPost("/[controller]/CreateAirplane")]
+        public Response CreateAirplane([FromBody] Request objRequestParams)
+        {
+            Response objResponse = new Response()
+            {
+                Resp = true
+            };
+            try
+            {
+                Connection objConnection = new Connection(Config.DBPass, Config.DBServer, Config.DBName, Config.DBUser);
+                JObject objRequestParam = JObject.Parse(objRequestParams.Data);
+
+                string StrName = objRequestParam["Name"].ToString();
+                string strCompanyID = objRequestParam["CompanyID"].ToString();
+
+                string[] arrParams = new string[] { "@CompanyID" };
+                string[] arrValues = new string[] { strCompanyID };
+
+                string strQuery = @"SELECT * FROM AirPlane WHERE CompanyID = @CompanyID";
+
+                ResponseDB dbResponse = objConnection.getRespFromQuery(0, 500, "", strQuery, arrParams, arrValues, "Name", "DataTable");
+
+                if (dbResponse.Resp)
+                {
+                    if (dbResponse.Count > 0)
+                    {
+                        JArray arrCities = JArray.Parse(JsonConvert.SerializeObject(dbResponse.dtResult, Formatting.None));
+
+                        foreach (JObject city in arrCities)
+                        {
+                            if (city["Name"].ToString().ToLower().Trim().Replace("á", "a").Replace("é", "e").Replace("í", "i").Replace("ó", "o").Replace("ú", "u").Equals(StrName.ToLower().Trim().Replace("á", "a").Replace("é", "e").Replace("í", "i").Replace("ó", "o").Replace("ú", "u")))
+                            {
+                                throw new Exception("Ya existe un avion con este nombre");
+                            }
+                        }
+                    }
+                   
+
+                    string strStatus = objRequestParam["Status"].ToString();
+                    string strCreatedby = objRequestParam["CreatedBy"].ToString();
+                    arrParams = new string[] { "@CompanyID", "@Name", "@Status", "@CreatedBy" };
+                    arrValues = new string[] { strCompanyID, StrName, strStatus, strCreatedby };
+
+                    strQuery = @"INSERT INTO AirPlane(CompanyID, Name, Status, CreatedBy) OUTPUT INSERTED.AirPlaneID 
+                                 VALUES (@CompanyID, @Name, @Status, @CreatedBy)";
+
+                    dbResponse = objConnection.InsData(strQuery, arrParams, arrValues);
+                    if (dbResponse.Resp)
+                    {
+                        if (dbResponse.Count > 0)
+                        {
+                            objResponse.Resp = true;
+                            objResponse.Type = "Success";
+                            objResponse.Msg = "Avion creado";
+                        }
+                        else
+                        {
+                            objResponse.Resp = false;
+                            objResponse.Type = "Error";
+                            objResponse.Msg = "No se pudo guardar el avion";
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception(dbResponse.Msg);
+                    }
+                }
+                else
+                {
+                    throw new Exception(dbResponse.Msg);
+                }
+
+            }
+            catch (Exception exc)
+            {
+                objResponse.Resp = false;
+                objResponse.Type = "Error";
+                objResponse.Msg = "Ocurrió un error inesperado, por favor consulte al administrador";
+                objResponse.Detail = exc.Message;
+            }
+
+            return objResponse;
+
+        }
+
+        [HttpPut("/[controller]/UpdateAirplane")]
+        public Response UpdateAirplane([FromBody] Request objRequestParams)
+        {
+            Response objResponse = new Response()
+            {
+                Resp = true
+            };
+            try
+            {
+                Connection objConnection = new Connection(Config.DBPass, Config.DBServer, Config.DBName, Config.DBUser);
+                JObject objRequestParam = JObject.Parse(objRequestParams.Data);
+
+                string strAirplaneID = objRequestParam["AirplaneID"].ToString();
+                string strCompanyID = objRequestParam["CompanyID"].ToString();
+                string strName = objRequestParam["Name"].ToString();
+                string strStatus = objRequestParam["Status"].ToString();
+                string strModifiedBy = objRequestParam["ModifiedBy"].ToString();
+
+
+                string[] arrParams = new string[] { "@AirplaneID", "@CompanyID", "@Name", "@Status", "@ModifiedBy" };
+                string[] arrValues = new string[] { strAirplaneID, strCompanyID, strName, strStatus, strModifiedBy };
+
+                string strQuery = @"UPDATE AirPlane SET Name = @Name, Status = @Status, CompanyID = @CompanyID, ModifiedBy = @ModifiedBy, ModificationDate = GETDATE() WHERE AirPlaneID = @AirplaneID";
+
+                ResponseDB dbResponse = objConnection.UpdData(strQuery, arrParams, arrValues);
+
+                if (dbResponse.Resp)
+                {
+                    if (dbResponse.Count > 0)
+                    {
+                        objResponse.Resp = true;
+                        objResponse.Type = "Success";
+                        objResponse.Msg = "Empresa actualizada";
+                    }
+                    else
+                    {
+                        objResponse.Resp = false;
+                        objResponse.Type = "Error";
+                        objResponse.Msg = "No se pudo actualizar la Empresa";
+                    }
+                }
+                else
+                {
+                    throw new Exception(dbResponse.Msg);
+                }
+            }
+            catch (Exception exc)
+            {
+                objResponse.Resp = false;
+                objResponse.Type = "Error";
+                objResponse.Msg = "Ocurrió un error inesperado, por favor consulte al administrador";
+                objResponse.Detail = exc.Message;
+            }
+            return objResponse;
+        }
+    }
+}
---
+++ AirLine/AirLineApi/Controllers/IntineraryController.cs
@@ -0,0 +1,266 @@
+﻿using AirLineApi.Models;
+using AirLineApi.Util;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Runtime.Serialization;
+
+namespace AirLineApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntineraryController : ControllerBase
+    {
+        [HttpGet("/[controller]/GetItineraries")]
+        public Response GetItineraries()
+        {
+            Response objResponse = new Response()
+            {
+                Resp = true
+            };
+            try
+            {
+                Connection objConnection = new Connection(Config.DBPass, Config.DBServer, Config.DBName, Config.DBUser);
+
+
+                string[] arrParams = new string[] { };
+                string[] arrValues = new string[] { };
+
+                string strQuery = @"SELECT 
+                                    	FLI.FlyIntineraryID, FLI.AirPlaneID, AIP.Name AS NAME_AIRPLAIN, 
+                                    	FLI.Origin, ORI.Name AS ORIGIN_NAME, FLI.Destiny, DEST.Name AS DESTINY_NAME,
+                                    	FLI.FlyDate, FLI.TakeOffTime, FLI.ArrivalDate, FLI.ArrivalTime, FLI.Status
+                                    FROM FlyIntinerary FLI
+                                    INNER JOIN AirPlane AIP ON FLI.AirPlaneID = AIP.AirPlaneID
+                                    LEFT JOIN City ORI ON FLI.Origin = ORI.CityID
+                                    LEFT JOIN City DEST ON FLI.Destiny = DEST.CityID";
+
+                ResponseDB dbResponse = objConnection.getRespFromQuery(0, 500, "", strQuery, arrParams, arrValues, "FLI.FlyDate DESC", "DataTable");
+
+                if (dbResponse.Resp)
+                {
+                    if (dbResponse.Count > 0)
+                    {
+                        objResponse.Count = dbResponse.Count;
+                        objResponse.Data = JsonConvert.SerializeObject(dbResponse.dtResult, Formatting.None);
+                    }
+                    else
+                    {
+                        objResponse.Resp = false;
+                        objResponse.Type = "Error";
+                        objResponse.Msg = "No se encontro ningun usuario asociado a tu empresa";
+                    }
+                }
+                else
+                {
+                    throw new Exception(dbResponse.Msg);
+                }
+
+            }
+            catch (Exception exc)
+            {
+                objResponse.Resp = false;
+                objResponse.Type = "Error";
+                objResponse.Msg = "Ocurrió un error inesperado, por favor consulte al administrador";
+                objResponse.Detail = exc.Message;
+            }
+
+            return objResponse;
+        }
+
+        [HttpPost("/[controller]/CreateItinerary")]
+        public Response CreateItinerary([FromBody] Request objRequestParams)
+        {
+            Response objResponse = new Response()
+            {
+                Resp = true
+            };
+            try
+            {
+                Connection objConnection = new Connection(Config.DBPass, Config.DBServer, Config.DBName, Config.DBUser);
+                JObject objRequestParam = JObject.Parse(objRequestParams.Data);
+
+                string strAirplaneID = objRequestParam["AirplaneID"].ToString();
+                string strTakeOffDate = objRequestParam["TakeOffDate"].ToString();
+                string strArrivalDate = objRequestParam["ArrivalDate"].ToString();
+                string strTakeOffTime = objRequestParam["TakeOffTime"].ToString();
+                string strArrivalTime = objRequestParam["ArrivalTime"].ToString();
+                string strOrigin = objRequestParam["Origin"].ToString();
+                string strDestiny = objRequestParam["Destiny"].ToString();
+                string strStatus = objRequestParam["Status"].ToString();
+                string strCreatedBy = objRequestParam["CreadtedBy"].ToString();
+
+                string[] arrParams = new string[] { "@AirplaneID", "@TakeOffDate", "@TakeOffTime", "@ArrivalDate", "@ArrivalTime", "@Origin", "@Destiny", "@Status", "@CreatedBy" };
+                string[] arrValues = new string[] { strAirplaneID, strTakeOffDate, strTakeOffTime, strArrivalDate, strArrivalTime, strOrigin, strDestiny, strStatus, strCreatedBy };
+
+                string strQuery = @"SELECT FlyIntineraryID 
+                                    FROM FlyIntinerary 
+                                    WHERE 
+                                    AirPlaneID = @AirplaneID 
+                                    AND (CAST(CONCAT(@TakeOffDate, ' ', @TakeOffTime) AS datetime) 
+                                    BETWEEN CAST(CONCAT(CAST(FlyDate as nvarchar), ' ', CONVERT(nvarchar, TakeOffTime, 8)) AS datetime) 
+                                    AND CAST(CONCAT(CAST(ArrivalDate as nvarchar), ' ', CONVERT(nvarchar, ArrivalTime, 8)) AS datetime) 
+                                    OR 
+                                    CAST(CONCAT(@ArrivalDate, ' ', @ArrivalTime) AS datetime) 
+                                    BETWEEN CAST(CONCAT(CAST(FlyDate as nvarchar), ' ', CONVERT(nvarchar, TakeOffTime, 8)) AS datetime) 
+                                    AND CAST(CONCAT(CAST(ArrivalDate as nvarchar), ' ', CONVERT(nvarchar, ArrivalTime, 8)) AS datetime))";
+
+                ResponseDB dbResponse = objConnection.getRespFromQuery(0, 500, "", strQuery, arrParams, arrValues, "FlyIntineraryID", "DataTable");
+                if (dbResponse.Resp)
+                {
+                    if (dbResponse.Count > 0)
+                    {
+                        objResponse.Resp = false;
+                        objResponse.Type = "Error";
+                        objResponse.Msg = "las fechas de vuelo se cruzan con otro intinerario";
+                    }
+                    else
+                    {
+                        strQuery = @"INSERT INTO FlyIntinerary (AirPlaneID, Origin, Destiny, FlyDate, TakeOffTime, ArrivalDate, ArrivalTime, Status, CreatedBy) OUTPUT INSERTED.FlyIntineraryID
+                                     VALUES (@AirplaneID, @Origin, @Destiny, @TakeOffDate, @TakeOffTime, @ArrivalDate, @ArrivalTime, @Status, @CreatedBy)";
+
+                        dbResponse = objConnection.InsData(strQuery, arrParams, arrValues);
+
+                        if (dbResponse.Resp)
+                        {
+                            if (dbResponse.Count > 0)
+                            {
+                                objResponse.Resp = true;
+                                objResponse.Type = "Success";
+                                objResponse.Msg = "vuelo creado";
+                            }
+                            else
+                            {
+                                objResponse.Resp = false;
+                                objResponse.Type = "Error";
+                                objResponse.Msg = "No se pudo guardar el vuelo";
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception(dbResponse.Msg);
+                        }
+                    }
+                }
+                else
+                {
+                    throw new Exception(dbResponse.Msg);
+                }
+
+
+            }
+            catch (Exception exc)
+            {
+                objResponse.Resp = false;
+                objResponse.Type = "Error";
+                objResponse.Msg = "Ocurrió un error inesperado, por favor consulte al administrador";
+                objResponse.Detail = exc.Message;
+            }
+
+            return objResponse;
+
+        }
+
+        [HttpGet("/[controller]/GetMostvisited")]
+        public Response GetMostvisited()
+        {
+            Response objResponse = new Response()
+            {
+                Resp = true
+            };
+            try
+            {
+                Connection objConnection = new Connection(Config.DBPass, Config.DBServer, Config.DBName, Config.DBUser);
+
+
+                string[] arrParams = new string[] { };
+                string[] arrValues = new string[] { };
+
+                string strQuery = @"EXEC MOST_VISITED_DESTINIES";
+
+                ResponseDB dbResponse = objConnection.getRespFromQuery(0, 500, "", strQuery, arrParams, arrValues, "NO_PAGINATE", "DataTable");
+
+                if (dbResponse.Resp)
+                {
+                    if (dbResponse.Count > 0)
+                    {
+                        objResponse.Count = dbResponse.Count;
+                        objResponse.Data = JsonConvert.SerializeObject(dbResponse.dtResult, Formatting.None);
+                    }
+                    else
+                    {
+                        objResponse.Resp = false;
+                        objResponse.Type = "Error";
+                        objResponse.Msg = "No se encontro ningun usuario asociado a tu empresa";
+                    }
+                }
+                else
+                {
+                    throw new Exception(dbResponse.Msg);
+                }
+
+            }
+            catch (Exception exc)
+            {
+                objResponse.Resp = false;
+                objResponse.Type = "Error";
+                objResponse.Msg = "Ocurrió un error inesperado, por favor consulte al administrador";
+                objResponse.Detail = exc.Message;
+            }
+
+            return objResponse;
+        }
+
+        [HttpGet("/[controller]/GetFliedHours")]
+        public Response GetFliedHours(string strAirPlaneID)
+        {
+            Response objResponse = new Response()
+            {
+                Resp = true
+            };
+            try
+            {
+                Connection objConnection = new Connection(Config.DBPass, Config.DBServer, Config.DBName, Config.DBUser);
+
+
+                string[] arrParams = new string[] { "@AirplaneID" };
+                string[] arrValues = new string[] { strAirPlaneID };
+
+                string strQuery = @"EXEC Airplane_Hours_Flown @AirPlane = @AirplaneID";
+
+                ResponseDB dbResponse = objConnection.getRespFromQuery(0, 500, "", strQuery, arrParams, arrValues, "NO_PAGINATE", "DataTable");
+
+                if (dbResponse.Resp)
+                {
+                    if (dbResponse.Count > 0)
+                    {
+                        objResponse.Count = dbResponse.Count;
+                        objResponse.Data = JsonConvert.SerializeObject(dbResponse.dtResult, Formatting.None);
+                    }
+                    else
+                    {
+                        objResponse.Resp = false;
+                        objResponse.Type = "Error";
+                        objResponse.Msg = "No se encontro ningun usuario asociado a tu empresa";
+                    }
+                }
+                else
+                {
+                    throw new Exception(dbResponse.Msg);
+                }
+
+            }
+            catch (Exception exc)
+            {
+                objResponse.Resp = false;
+                objResponse.Type = "Error";
+                objResponse.Msg = "Ocurrió un error inesperado, por favor consulte al administrador";
+                objResponse.Detail = exc.Message;
+            }
+
+            return objResponse;
+        }
+    }
+}
